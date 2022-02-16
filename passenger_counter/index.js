7aeffad9be9d7766733755425d6a187a9d545667
pratikgirikonvergedotai <pratik.giri@konverge.ai>
2022-02-16 16:26:25 +0530

    passenger counter project added

---
+++ passenger_counter/index.js
@@ -0,0 +1,16 @@
+let count = 0
+
+function increment(){
+    count += 1
+    //document.getElementById("count-el").innerText = count
+    document.getElementById("count-el").textContent = count
+}
+
+function save(){ 
+    let saved_count = count + " - "
+    //document.getElementById("save-el").innerText += " " + saved_count
+    //innerText can be computational expensive than textContent
+    document.getElementById("save-el").textContent += saved_count
+    document.getElementById("count-el").textContent = 0
+    count = 0
+}
