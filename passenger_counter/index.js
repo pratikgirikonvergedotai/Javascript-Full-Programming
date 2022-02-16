41a037988d63c793d49f217b97073c6f7a4d172b
pratikgirikonvergedotai <pratik.giri@konverge.ai>
2022-02-16 16:34:51 +0530

    passenger counter project added

--- passenger_counter/index.js
+++ passenger_counter/index.js
@@ -9,7 +9,7 @@ function increment(){
 function save(){ 
     let saved_count = count + " - "
     //document.getElementById("save-el").innerText += " " + saved_count
-    //innerText can be computational expensive than textContent
+    //innerText can be computationally expensive than textContent
     document.getElementById("save-el").textContent += saved_count
     document.getElementById("count-el").textContent = 0
     count = 0
