adf44585e19c82061ce1dba43fe6b7b647206b2c
pratikgirikonvergedotai <pratik.giri@konverge.ai>
2022-02-18 12:04:45 +0530

    chrome extension

--- chrome_extension/index.js
+++ chrome_extension/index.js
@@ -1,4 +1,4 @@
-let myLeads = []
+let Leads = []
 const inputEl = document.getElementById("input-el")
 
 const inputBtn = document.getElementById("input-btn")
@@ -7,7 +7,7 @@ const deleteBtn = document.getElementById("delete-btn")
 const tabBtn = document.getElementById("tab-btn")
 
 // getting localstorage data and parsing string as localstorage only alows string
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("Leads"))
 
 
 
@@ -16,16 +16,16 @@ const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 // checking for null value in localstorage
 if (leadsFromLocalStorage){
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
+    Leads = leadsFromLocalStorage
+    render(Leads)
 }
 
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
+    Leads.push(inputEl.value)
     inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    render(myLeads)
+    localStorage.setItem("myLeads", JSON.stringify(Leads))
+    render(Leads)
 })
 
 function render(leads){
@@ -43,8 +43,8 @@ function render(leads){
 // we are listening the double click event
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
-    myLeads = []
-    render(myLeads)
+    Leads = []
+    render(Leads)
 })
 
 tabBtn.addEventListener("click", function(){
@@ -52,12 +52,12 @@ tabBtn.addEventListener("click", function(){
     // tabs is a array with a object that a key named url
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads))
-        render(myLeads)
+        Leads.push(tabs[0].url)
+        localStorage.setItem("Leads", JSON.stringify(Leads))
+        render(Leads)
     })
     
-    myLeads.push(tabs[0].url)
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    render(myLeads)
+    Leads.push(tabs[0].url)
+    localStorage.setItem("Leads", JSON.stringify(Leads))
+    render(Leads)
 })
